b798184606a7972ee01251826bcd569da3e7e4e1
S0ULB0Y <dwikavindra.haryo.r@mail.ugm.ac.id>
2021-03-08 21:33:04 +0700

    First Commit

---
+++ app/src/main/java/com/example/navigationcomponent/home/UserRecyclerAdapter.kt
@@ -0,0 +1,20 @@
+package com.example.navigationcomponent.home
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class UserRecyclerAdapter:RecyclerView.Adapter<UserRecyclerAdapter.UserViewHolder>() {
+    inner class UserViewHolder(itemView:View):RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
